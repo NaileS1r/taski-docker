3e29c861f433de9b41dc9af11dc36b573d6a6bad
NaileS1r <sirazitdinov.naile@yandex.ru>
2024-08-22 15:06:14 +0300

    fix domain in settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,8 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1', 'localhost:8000', '51.250.96.222']
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '51.250.96.222', 'nailedomain.zapto.org']
 
 # Application definition
 
